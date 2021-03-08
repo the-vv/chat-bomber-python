4c16e8b748f59c8992aca318714498f3f7241d5e
vv-2001 <vishnuvinod2772001@gmail.com>
2021-03-08 19:15:26 +0530

    stable working simple version

---
+++ bomber.py
@@ -0,0 +1,23 @@
+import pyautogui as pg
+import time
+import os
+
+word = input('Enter message to send: ')
+num = input('Enter number of times to send: ')
+print('Place the cursor where you want to send messages')
+print("sending in 10 secs")
+for i in range(10):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('Place the cursor where you want to send messages')
+    print('Sending starts in', 10 - i, 'seconds')
+    time.sleep(1)
+count = 0
+for i in range(int(num)):
+    pg.write(word)
+    pg.press('enter')
+    count += 1
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('Sending ' + num + ' ' + '\'' + word + '\'' + '\nTotal Send:', count)
+
+os.system('cls' if os.name == 'nt' else 'clear')
+input('Operation Completed, press enter to exit...')
